12cff666beeff9e1a6fc9d6587d56bc1ae555f90
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2024-04-19 10:13:43 -0300

    feat: adiciona funcionalidade de tabs para gerenciar motoristas e veículos
    Esta atualização introduz uma interface com guias para facilitar a navegação e gestão de informações relacionadas a motoristas e veículos. Agora os usuários podem alternar facilmente entre as guias 'Motorista' e 'Veículo' para visualizar e editar dados específicos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import './App.css';
+import React from "react";
+import BasicTabs from "./components/tabs/Tabs";
 
 function App() {
-
   return (
-    <h1>Fioravante</h1>
+    <BasicTabs>
+      <h1>Content</h1>
+    </BasicTabs>
   )
 }
 
